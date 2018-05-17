d523ecd81874e940698750eea23131d8fa9fad15
Stepanenko Vlad <vladstepanenko1@hotmail.com>
2018-05-17 21:08:13 +0300

    Add AirportService

---
+++ BookingSystem.BL/Services/AirportService.cs
@@ -0,0 +1,46 @@
+﻿using BookingSystem.BL.Interfaces;
+using BookingSystem.DAL.Domain;
+using BookingSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.BL.Services
+{
+    public class AirportService : IAirportService
+    {
+        private IRepository<Airport> _airportRepository;
+
+        public AirportService(IRepository<Airport> airportRepository)
+        {
+            _airportRepository = airportRepository;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id, string name, string address, string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Airport Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Airport> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(int id, string name, string address, string country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
