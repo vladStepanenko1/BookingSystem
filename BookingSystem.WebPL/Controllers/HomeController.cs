da2c1b2d87e9d00c33e5c6a3c13ec4d05a73f51a
Stepanenko Vlad <vladstepanenko1@hotmail.com>
2018-05-21 23:11:22 +0300

    Add Web interface

---
+++ BookingSystem.WebPL/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using BookingSystem.BL.Interfaces;
+using BookingSystem.DAL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSystem.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAirportService airportService;
+
+        public HomeController(IAirportService service)
+        {
+            airportService = service;
+        }
+
+        public ActionResult Index()
+        {
+            var airports = airportService.GetAll();
+            return View(airports);
+        }
+    }
+}
