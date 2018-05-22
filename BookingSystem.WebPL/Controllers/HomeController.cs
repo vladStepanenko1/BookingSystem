8dae59d0e72dce570932c5d1d0858158c1e44b83
Stepanenko Vlad <vladstepanenko1@hotmail.com>
2018-05-22 21:04:13 +0300

    Small fixies

--- BookingSystem.WebPL/Controllers/HomeController.cs
+++ BookingSystem.WebPL/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
 ﻿using BookingSystem.BL.Interfaces;
-using BookingSystem.DAL.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BookingSystem.Web.Controllers
