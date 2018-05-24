d6dfad52231b650b08d864d5d30ccb3c686696a5
Stepanenko Vlad <vladstepanenko1@hotmail.com>
2018-05-25 00:24:27 +0300

    Add Show page

--- BookingSystem.WebPL/Controllers/HomeController.cs
+++ BookingSystem.WebPL/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using BookingSystem.BL.Interfaces;
+﻿using BookingSystem.BL.DTO;
+using BookingSystem.BL.Interfaces;
+using BookingSystem.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace BookingSystem.Web.Controllers
@@ -14,8 +19,36 @@ namespace BookingSystem.Web.Controllers
 
         public ActionResult Index()
         {
-            var airports = airportService.GetAll();
+            IEnumerable<AirportDTO> airportDTOs = airportService.GetAll();
+            var airports = airportDTOs.Select(a => new AirportViewModel
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Address = a.Address,
+                Country = a.Country
+            });
             return View(airports);
+        }
+
+        public ActionResult Show(int id)
+        {
+            try
+            {
+                AirportDTO airportDTO = airportService.Get(id);
+                AirportViewModel airport = new AirportViewModel
+                {
+                    Id = airportDTO.Id,
+                    Name = airportDTO.Name,
+                    Address = airportDTO.Address,
+                    Country = airportDTO.Country
+                };
+                return View(airport);
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("Error", ex);
+            }
+            return View();
         }
     }
 }
---
+++ BookingSystem.WebPL/Models/AirportViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookingSystem.Web.Models
+{
+    public class AirportViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+
+        public AirportViewModel()
+        {
+
+        }
+    }
+}
